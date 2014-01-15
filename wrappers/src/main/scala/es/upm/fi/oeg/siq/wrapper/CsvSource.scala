48e5792b24e8ab1d60347f902956baca971b5af9
Alejandro Llaves <allaves@delicias.dia.fi.upm.es>
2014-01-15 19:06:40 +0100

    Support for HTTP responses in CSV format.

--- wrappers/src/main/scala/es/upm/fi/oeg/siq/wrapper/CsvSource.scala
+++ wrappers/src/main/scala/es/upm/fi/oeg/siq/wrapper/CsvSource.scala
@@ -2,37 +2,43 @@ package es.upm.fi.oeg.siq.wrapper
 
 import java.util.Date
 import scala.io.Source
+import java.io
 import dispatch._
 import com.ning.http.client.RequestBuilder
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import sys.process._
 
 class CsvSource(who:PollWrapper,id:String) extends Datasource(who,id){      
   val idname=who.idkeys(id)
-  val theurl=who.url.replace("{id}",id)
+  //val the url=who.url.replace("{id}",id)
+  var theurl=who.url.replace("{id}",id)
   lazy val rowrate=who.configvals("rowrate").toLong
   lazy val values=who.configvals("values").split(',').map{v=>new Func(v)}
   val funs = values.map{v=>v.instantiate}
 
-  val data={
-    /*
-     * Extension of the CSV wrappers for online resources
-     */ 
-    val d =
-    if (theurl.startsWith("http://")) {
-      val svc = url(theurl)
-      val res = Http(svc OK as.String)
-      val data = res()
-      Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(data)).bufferedReader
-    }
-    /*
-     * 
-     */
-    else {
-      Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(theurl)).bufferedReader
-    }
-    val dat=Stream.continually(d.readLine()).takeWhile(_!=null).map(extract(_))
-    //d.close
-    dat
+  val data = {
+	  /*
+	   * Extension of the CSV wrappers for online resources
+	   */ 
+	  if (theurl.startsWith("http://")) {
+	    val svc = url(theurl)
+		val res = Http(svc OK as.String)
+		val data = res()
+		// The response is right, but the file is not created!
+		print(data)
+		data #> new java.io.File("data/hsl.csv")
+		theurl = "/data/hsl.csv"
+	  }
+	  /*
+	   * 
+	   */
+	  val d = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(theurl)).bufferedReader
+	  val dat=Stream.continually(d.readLine()).takeWhile(_!=null).map(extract(_))
+	  //d.close
+	  dat
   }
+  
   override def afterPosting={
     if (rowrate>0) Thread.sleep(rowrate)
   }
